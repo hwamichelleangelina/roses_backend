0d58460260a2c9308a6a23ca9818bccf6b6f245a
18320007 Michelle Angelina <hwamichelleangelina@gmail.com>
2024-06-03 19:59:35 +0700

    API Vercel Testing

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.use('/auth', routerUser);
 app.use('/messages', routerMessage);
 app.use('/roses', routerRose);
 
+app.get('/', async (req, res) => {
+    res.json({ message: 'This is Ehthereal BlossomBox API!' });
+});
+
 // Start the server
 app.listen(3000, () => {
     console.log('Server running on port 3000');
